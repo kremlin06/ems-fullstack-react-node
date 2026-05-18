8536cd1a28b280e5591e64d595952d81a4b94ced
Kremlin <jhonkenth1307@gmail.com>
2026-05-18 09:10:42 +0800

    Update useAuth.js

--- client/src/context/useAuth.js
+++ client/src/context/useAuth.js
@@ -6,6 +6,6 @@ import AuthContext from './AuthContext'
 // the error check ensures you don't use this outside the provider, which would break
 export const useAuth = () => {
    const context = useContext(AuthContext);
-   if (!context) throw new Error('useAuth must be used within AuthProvider');
+   if (!context) throw new Error('useAuth must be used within AuthProvider.' + 'Wrap your component tree with <AuthProvider>.');
    return context;
 };
